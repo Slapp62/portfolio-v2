42dc561fe51fcf0f03760272096f74c2946a137e
Simcha <slapp62@gmail.com>
2025-09-18 22:32:29 +0300

    header tweak

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -3,10 +3,12 @@ import { IconBrandLinkedin, IconBrandWhatsapp, IconMail } from "@tabler/icons-re
 export const Header = () => {
   return (
     <header className="header w-full text-gray-400">
-      <nav className="text-xl mt-2 gap-10 flex justify-end align-center mr-5">
-        <button><a className="link-hover scroll-smooth" href="#projects">Projects</a></button>
-        <button><a className="link-hover scroll-smooth" href="#contact">Contact</a></button>
-        
+      <nav className="text-xl mt-2 gap-10 flex justify-between align-center mr-5">
+        <div className="flex gap-10 px-10">
+          <button><a className="link-hover scroll-smooth" href="#projects">Projects</a></button>
+          <button><a className="link-hover scroll-smooth" href="#contact">Contact</a></button>
+        </div>
+
         <div className="flex gap-5 items-center justify-center border border-gray-400 rounded-lg py-2 px-3">
           <a 
             className="link-hover" 
