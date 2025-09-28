e331634669f37006a0a92de5d242f8ded1bdeb81
Simcha <slapp62@gmail.com>
2025-09-28 14:22:59 +0300

    back to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import ProjectSection from './components/sections/ProjectSection.tsx';
 import { Hero } from './components/sections/Hero.tsx';
 import { Footer } from './components/navigation/Footer.tsx';
 import { Contact } from './components/sections/ContactForm.tsx';
+import { Button } from './components/ui/button.tsx';
 
 function App() {
   return (
-    <div className="flex flex-col gap-10 justify-center lg:max-w-full lg:flex-row">
+    <div id='app' className="flex flex-col gap-10 justify-center lg:max-w-full lg:flex-row">
       <Hero />
       <div className="mx-auto flex max-w-[90%] flex-col gap-10 lg:mx-0 lg:mt-15 lg:ml-10 lg:w-[40%]">
-        
         <ProjectSection />
         <Contact />
+        <Button className='w-fit px-10 mx-auto'><a href="#app">Back to Top</a></Button>
         <Footer />
       </div>
     </div>
